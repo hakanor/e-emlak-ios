b1663e0cfcdf8136dcad5a5210b2f528b425653a
hakanor <hakanor99@gmail.com>
2022-10-03 14:17:53 +0300

    Initial Commit

---
+++ e-emlak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  e-emlak
+//
+//  Created by Hakan Or on 3.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
