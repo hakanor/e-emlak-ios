fa532779363c4cd728928feab4ce3d33aecd9e91
hakanor <hakanor99@gmail.com>
2022-10-14 17:50:12 +0300

    Added IQKeyboardManager

--- e-emlak/AppDelegate.swift
+++ e-emlak/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.toolbarDoneBarButtonItemText = "Tamam"
         // Override point for customization after application launch.
         return true
     }
