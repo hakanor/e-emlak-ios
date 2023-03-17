4cf3135fcd2079f4bc3c2d91baf344fb14e1cf64
hakanor <hakanor99@gmail.com>
2023-03-17 18:34:01 +0300

    Added MapModeViewController

---
+++ e-emlak/Controller/Map/MapModeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapModeViewController.swift
+//  e-emlak
+//
+//  Created by Hakan Or on 13.03.2023.
+//
+
+import UIKit
+
+class MapModeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
